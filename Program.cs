5cf0280d5e5662cb58b4cd4b58f6a3c9b1d55c70
unknown <donofrio.8@wright.edu>
2023-03-24 15:22:23 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declaring Variables
+string stringVariable = "string";
+int intagerVariable = 0;
+char characterVariable = 'a';
+bool booleanVariable = false;
+double doubleVariable = 0.0;
+decimal decimalVariable = 0.00m;
+
+// Write to the console
+Console.WriteLine($"Name: {stringVariable}");
+Console.WriteLine($"Age: {intagerVariable}");
+Console.WriteLine($"Initial: {characterVariable}");
+Console.WriteLine($"18 or Over: {booleanVariable}");
+Console.WriteLine($"Temp: {doubleVariable}");
+Console.WriteLine($"Price: {decimalVariable}");
